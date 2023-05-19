22541cb06754a8a576bdc3f07f7460f558b6b3f0
Andrew Lin <22057003+andrewlin94@users.noreply.github.com>
2023-05-18 23:27:41 -0400

    Fix errors

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,8 @@
-import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
-import { Button } from "@mui/material/";
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
@@ -19,11 +16,6 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <Button
-          children={
-            <div>Shit</div>
-          }
-        ></Button>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
